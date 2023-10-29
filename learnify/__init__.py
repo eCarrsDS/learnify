c6ef243c0a150bbc35ad07c00dd1bb4e89a8a677
Elton.C <eltonic40@gmail.com>
2023-10-28 21:19:49 -0700

    clean up configuration

--- learnify/__init__.py
+++ learnify/__init__.py
@@ -2,4 +2,6 @@
 One-line description for README and other doc files.
 """
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
+
+default_app_config = 'learnify.apps.LearnifyConfig'
--- learnify/apps.py
+++ learnify/apps.py
@@ -11,6 +11,7 @@ class LearnifyConfig(AppConfig):
     """
 
     name = 'learnify'
+    verbose_name = 'Learnify'
 
     plugin_app = {
         'url_config': {
@@ -24,22 +25,6 @@ class LearnifyConfig(AppConfig):
             'lms.djangoapp': {
                 'production': { 'relative_path': 'settings.production' },
                 'common': { 'relative_path': 'settings.common' },
-            }
-        },
-        'signals_config': {
-            'lms.djangoapp': {
-                'relative_path': 'my_signals',
-                'receivers': [{
-                    'receiver_func_name': 'on_signal_x',
-                    'signal_path': 'full_path_to_signal_x_module.SignalX',
-                    'dispatch_uid': 'learnify.my_signals.on_signal_x',
-                    'sender_path': 'full_path_to_sender_app.ModelZ',
-                }],
-            }
-        },
-        'view_context_config': {
-            'lms.djangoapp': {
-                'course_dashboard': 'learnify.context_api.get_dashboard_context'
             }
         }
     }
