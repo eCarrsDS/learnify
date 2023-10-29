06b25da13333305653e7e333c08fdcf9ae4c738d
Elton.C <eltonic40@gmail.com>
2023-10-28 18:15:07 -0700

    Bootstrap

---
+++ learnify/apps.py
@@ -0,0 +1,13 @@
+"""
+learnify Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class LearnifyConfig(AppConfig):
+    """
+    Configuration for the learnify Django application.
+    """
+
+    name = 'learnify'
